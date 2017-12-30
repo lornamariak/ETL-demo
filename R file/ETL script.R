b30c8001cffc0da263318da271a232a969de8d37
Lorna Maria <lorna1661maria@gmail.com>
2017-12-31 00:28:09 +0300

    added R script

---
+++ R file/ETL script.R
@@ -0,0 +1,27 @@
+#import excel file 
+library(readr)
+ 
+n <- as.integer(readline(prompt="choose file extension : 1.csv,2.txt"))
+if (n==1){
+  datafile <- read.table(file.choose(),header = TRUE ,sep=",")
+  View(datafile)
+}else if (n==2){
+  datafile <- read.table(file.choose(),header = TRUE ,sep="\t")
+  View(datafile)
+}else {
+  print("choose valid output")
+  }
+
+# insert analysis here
+
+
+#export data
+
+
+library("RSQLite")
+sqlite    <- dbDriver("SQLite")
+mydb <- dbConnect(sqlite, dbname="my.db")
+dbListTables(mydb)
+getwd()
+dbWriteTable(mydb,"table1",datafile)
+dbListFields(mydb, "table1")
